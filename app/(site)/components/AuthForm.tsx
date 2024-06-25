038b8b95183c643804306abda90afe9ddcd508e3
Aksel Glyholt <me@akselglyholt.com>
2024-06-25 12:16:48 +0200

    Started work on sidebar

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 
 import { Button } from "@/app/components/Button";
@@ -9,13 +9,22 @@ import { AuthSocialButton } from "./AuthSocialButton";
 import { BsGithub, BsGoogle } from "react-icons/bs";
 import axios from "axios";
 import toast from "react-hot-toast";
-import { signIn } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 type Variant = "LOGIN" | "REGISTER";
 
 function AuthForm() {
+  const session = useSession();
+  const router = useRouter();
   const [variant, setVariant] = useState<Variant>("LOGIN");
   const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (session?.status === "authenticated") {
+      router.push("/users");
+    }
+  }, [session?.status, router]);
 
   const toggleVariant = useCallback(() => {
     console.log("CLICKED");
@@ -42,8 +51,11 @@ function AuthForm() {
     if (variant === "REGISTER") {
       axios
         .post(`/api/register`, data)
+        .then(() => signIn("credentials", data))
         .catch(() => toast.error("Something went wrong!"))
-        .finally(() => setIsLoading(false));
+        .finally(() => {
+          setIsLoading(false);
+        });
     }
 
     if (variant === "LOGIN") {
@@ -58,6 +70,7 @@ function AuthForm() {
 
           if (callback?.ok && !callback?.error) {
             toast.success("Logged in!");
+            router.push("/users");
           }
         })
         .finally(() => {
---
+++ app/components/sidebar/MobileItem.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import Link from "next/link";
+import clsx from "clsx";
+
+interface MobileItemProps {
+  href: string;
+  icon: any;
+  active?: boolean;
+  onClick?: () => void;
+}
+
+const MobileItem: React.FC<MobileItemProps> = ({}) => {
+  return <div>MobileItem</div>;
+}
+
+export default MobileItem;
